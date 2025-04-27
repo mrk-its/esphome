a0d536f3f55d1c2f73bba5d6c14f5802269d52eb
Mariusz Kryński <mrk@sed.pl>
2025-04-27 21:07:34 +0000

    log clock config config

--- esphome/components/stm32/core.cpp
+++ esphome/components/stm32/core.cpp
@@ -1,5 +1,8 @@
 #ifdef USE_STM32
 #include "core.h"
+#include "esphome/core/log.h"
+
+const char *TAG = "main";
 
 // extern IWDG_HandleTypeDef hiwdg;
 
@@ -8,6 +11,111 @@ extern void loop();
 
 void init_uart();
 void uart_write_str(const char *str);
+
+// Helper functions to get prescaler values
+uint32_t get_ahb_prescaler(uint32_t divider) {
+  switch (divider) {
+    case RCC_SYSCLK_DIV1: return 1;
+    case RCC_SYSCLK_DIV2: return 2;
+    case RCC_SYSCLK_DIV4: return 4;
+    case RCC_SYSCLK_DIV8: return 8;
+    case RCC_SYSCLK_DIV16: return 16;
+    case RCC_SYSCLK_DIV64: return 64;
+    case RCC_SYSCLK_DIV128: return 128;
+    case RCC_SYSCLK_DIV256: return 256;
+    case RCC_SYSCLK_DIV512: return 512;
+    default: return 1;
+  }
+}
+
+uint32_t get_apb_prescaler(uint32_t divider) {
+  switch (divider) {
+    case RCC_HCLK_DIV1: return 1;
+    case RCC_HCLK_DIV2: return 2;
+    case RCC_HCLK_DIV4: return 4;
+    case RCC_HCLK_DIV8: return 8;
+    case RCC_HCLK_DIV16: return 16;
+    default: return 1;
+  }
+}
+void log_clock_config() {
+  RCC_ClkInitTypeDef clkinitstruct = {0};
+  RCC_OscInitTypeDef oscinitstruct = {0};
+
+  uint32_t uwPLLSource = 0;
+  uint32_t sysClockFreq = HAL_RCC_GetSysClockFreq();
+  uint32_t hclkFreq = HAL_RCC_GetHCLKFreq();
+  uint32_t pclk1Freq = HAL_RCC_GetPCLK1Freq();
+  uint32_t pclk2Freq = HAL_RCC_GetPCLK2Freq();
+
+  HAL_RCC_GetClockConfig(&clkinitstruct, &uwPLLSource);
+  HAL_RCC_GetOscConfig(&oscinitstruct);
+
+  const char* pllSourceStr;
+  switch (uwPLLSource) {
+    case RCC_PLLSOURCE_NONE:
+      pllSourceStr = "None";
+      break;
+    case RCC_PLLSOURCE_HSI:
+      pllSourceStr = "HSI";
+      break;
+    case RCC_PLLSOURCE_HSE:
+      pllSourceStr = "HSE";
+      break;
+    default:
+      pllSourceStr = "Unknown";
+      break;
+  }
+
+  const char *oscillatorTypeStr;
+  switch (oscinitstruct.OscillatorType) {
+    case RCC_OSCILLATORTYPE_HSE:
+      oscillatorTypeStr = "HSE";
+      break;
+    case RCC_OSCILLATORTYPE_HSI:
+      oscillatorTypeStr = "HSI";
+      break;
+    case RCC_OSCILLATORTYPE_LSE:
+      oscillatorTypeStr = "LSE";
+      break;
+    case RCC_OSCILLATORTYPE_LSI:
+      oscillatorTypeStr = "LSI";
+      break;
+    default:
+      oscillatorTypeStr = "NONE";
+  }
+
+  ESP_LOGI(TAG, "--- Clock Configuration ---");
+  ESP_LOGI(TAG, "System Clock Frequency (SYSCLK): %lu Hz (max: %lu Hz)", sysClockFreq, F_CPU);
+  ESP_LOGI(TAG, "HCLK Frequency (AHB Bus): %lu Hz", hclkFreq);
+  ESP_LOGI(TAG, "PCLK1 Frequency (APB1 Bus): %lu Hz", pclk1Freq);
+  ESP_LOGI(TAG, "PCLK2 Frequency (APB2 Bus): %lu Hz", pclk2Freq);
+  ESP_LOGI(TAG, "--- Oscillators ---");
+  ESP_LOGI(TAG, "Oscilator Type: %s", oscillatorTypeStr);
+  ESP_LOGI(TAG, "HSI State: %s, Calibration Value: %lu", (oscinitstruct.HSIState == RCC_HSI_ON) ? "ON" : "OFF",
+           oscinitstruct.HSICalibrationValue);
+  ESP_LOGI(TAG, "HSE State: %s", (oscinitstruct.HSEState == RCC_HSE_ON) ? "ON" : (oscinitstruct.HSEState == RCC_HSE_BYPASS) ? "BYPASS" : "OFF");
+  ESP_LOGI(TAG, "LSE State: %s", (oscinitstruct.LSEState == RCC_LSE_ON) ? "ON" : (oscinitstruct.LSEState == RCC_LSE_BYPASS) ? "BYPASS" : "OFF");
+  ESP_LOGI(TAG, "LSI State: %s", (oscinitstruct.LSIState == RCC_LSI_ON) ? "ON" : "OFF");
+  ESP_LOGI(TAG, "PLL Source: %s", pllSourceStr);
+  if (oscinitstruct.PLL.PLLState == RCC_PLL_ON) {
+    ESP_LOGI(TAG, "--- PLL Configuration ---");
+    ESP_LOGI(TAG, "PLL Multiplier (N): %lu", oscinitstruct.PLL.PLLN);
+    ESP_LOGI(TAG, "PLL Divider (M): %lu", oscinitstruct.PLL.PLLM);
+    ESP_LOGI(TAG, "PLL P Divider (P): %lu", oscinitstruct.PLL.PLLP);
+    ESP_LOGI(TAG, "PLL Q Divider (Q): %lu", oscinitstruct.PLL.PLLQ);
+    ESP_LOGI(TAG, "PLL R Divider (R): %lu", oscinitstruct.PLL.PLLR);
+  }
+
+  uint32_t ahb_div = get_ahb_prescaler(clkinitstruct.AHBCLKDivider);
+  uint32_t apb1_div = get_apb_prescaler(clkinitstruct.APB1CLKDivider);
+  uint32_t apb2_div = get_apb_prescaler(clkinitstruct.APB2CLKDivider);
+
+  ESP_LOGI(TAG, "AHB Prescaler: %lu", ahb_div);
+  ESP_LOGI(TAG, "APB1 Prescaler: %lu", apb1_div);
+  ESP_LOGI(TAG, "APB2 Prescaler: %lu", apb2_div);
+
+}
 
 int main() {
   HAL_Init();
@@ -37,6 +145,8 @@ int main() {
   // #endif
 
   setup();
+
+  log_clock_config();
 
   while (1) {
     loop();
