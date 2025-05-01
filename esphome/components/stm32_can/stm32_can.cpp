3d6911f8a1f02764bceab053120ed421f9728d36
Mariusz Kryński <mrk@sed.pl>
2025-05-01 16:35:46 +0000

    compute can params runtime

--- esphome/components/stm32_can/stm32_can.cpp
+++ esphome/components/stm32_can/stm32_can.cpp
@@ -30,13 +30,18 @@ void STM32Can::setup() {
 //   }
 // }
 
+const uint32_t CAN_BITRATES[] = {
+    1000,  5000,  10000, 12500,  16000,  20000,  25000,  31250,  33000,  40000,   50000,
+    80000, 83300, 95000, 100000, 125000, 200000, 250000, 500000, 800000, 1000000,
+};
+
+static_assert((canbus::CanSpeed::CAN_1000KBPS + 1 == sizeof(CAN_BITRATES) / 4));
+
 bool STM32Can::setup_internal() {
   CAN_FilterTypeDef filter = {0};
   GPIO_InitTypeDef GPIO_InitStruct = {0};
 
-  // for 125kbit/s
   hcan.Instance = CAN1;
-  hcan.Init.Prescaler = 40;
   hcan.Init.Mode = CAN_MODE_NORMAL;
   hcan.Init.SyncJumpWidth = CAN_SJW_1TQ;
   hcan.Init.TimeSeg1 = CAN_BS1_13TQ;
@@ -47,6 +52,34 @@ bool STM32Can::setup_internal() {
   hcan.Init.AutoRetransmission = DISABLE;
   hcan.Init.ReceiveFifoLocked = DISABLE;
   hcan.Init.TransmitFifoPriority = DISABLE;
+
+  uint32_t bitrate = CAN_BITRATES[bit_rate_];
+  uint32_t sys_freq = HAL_RCC_GetSysClockFreq();
+
+  if (sys_freq % bitrate) {
+    ESP_LOGE(TAG, "sys_freq (%lu Hz) must be a multiply of requested bitrate (%lu bps)", sys_freq, bitrate);
+    return false;
+  }
+  uint32_t prescaller_tq = sys_freq / bitrate;
+  if (!(prescaller_tq % 16)) {
+    hcan.Init.Prescaler = prescaller_tq / 16;
+    hcan.Init.TimeSeg1 = CAN_BS1_13TQ;
+    hcan.Init.TimeSeg2 = CAN_BS2_2TQ;
+  } else if (!(prescaller_tq % 8)) {
+    hcan.Init.Prescaler = prescaller_tq / 8;
+    hcan.Init.TimeSeg1 = CAN_BS1_6TQ;
+    hcan.Init.TimeSeg2 = CAN_BS2_1TQ;
+  } else if (!(prescaller_tq % 4)) {
+    hcan.Init.Prescaler = prescaller_tq / 4;
+    hcan.Init.TimeSeg1 = CAN_BS1_2TQ;
+    hcan.Init.TimeSeg2 = CAN_BS2_1TQ;
+  } else {
+    ESP_LOGE(TAG, "cannot setup timings for sys_freq (%lu Hz) and bitrate (%lu bps)", sys_freq, bitrate);
+    return false;
+  }
+  ESP_LOGCONFIG(TAG, "prescaller: %lu, tseg1: %lx, tseg2: %lx", hcan.Init.Prescaler, hcan.Init.TimeSeg1,
+                hcan.Init.TimeSeg2);
+
   HAL_CAN_MspInit(&hcan);
   __HAL_RCC_CAN1_CLK_ENABLE();
   __HAL_RCC_GPIOA_CLK_ENABLE();
@@ -59,7 +92,7 @@ bool STM32Can::setup_internal() {
   HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
   if (HAL_CAN_Init(&hcan) == HAL_OK) {
     // TODO: make sure ErrorCode is 0 and State is READY
-    ESP_LOGI(TAG, "CAN: Initialized, state: %d, err: %d", hcan.State, hcan.ErrorCode);
+    ESP_LOGI(TAG, "CAN: Initialized, state: %d, err: %lu", hcan.State, hcan.ErrorCode);
 
     filter.FilterActivation = CAN_FILTER_ENABLE;
     filter.FilterScale = CAN_FILTERSCALE_16BIT;
@@ -99,7 +132,7 @@ canbus::Error STM32Can::send_message(struct canbus::CanFrame *frame) {
     ESP_LOGE(TAG, "output queues are full");
     return canbus::ERROR_ALLTXBUSY;
   }
-  ESP_LOGI(TAG, "free tx buffers: %ld", free_tx);
+  ESP_LOGV(TAG, "free tx buffers: %ld", free_tx);
   if (HAL_CAN_AddTxMessage(&hcan, &TxHeader, frame->data, &TxMailbox) == HAL_OK) {
     ESP_LOGV(TAG, "message sent, state: %ld, err: %ld", hcan.State, hcan.ErrorCode);
   } else {
