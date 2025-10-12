5ea2c59797603fc9cda32141853411729d8644d0
Mariusz Kryński <mrk@sed.pl>
2025-10-12 12:08:39 +0000

    move can_ring_buffer.h

--- esphome/components/stm32_fdcan/stm32_fdcan.h
+++ esphome/components/stm32_fdcan/stm32_fdcan.h
@@ -3,7 +3,7 @@
 
 #include "esphome/core/component.h"
 #include "esphome/core/defines.h"
-#include "esphome/components/stm32/can_ring_buffer.h"
+#include "can_ring_buffer.h"
 
 namespace esphome {
 namespace stm32_fdcan {
