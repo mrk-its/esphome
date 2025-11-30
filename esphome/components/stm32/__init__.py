aec80f81e627dd3545d8b1960cc8b557daac029e
Mariusz Kryński <mrk@sed.pl>
2025-11-30 20:12:17 +0100

    define second dma channel for F1

--- esphome/components/stm32/__init__.py
+++ esphome/components/stm32/__init__.py
@@ -51,7 +51,7 @@ def set_core_data(config):
         "LPUART1",
     ]
     if config[CONF_BOARD_SERIES] in ("F1", "L4"):
-        CORE.data[KEY_STM32][KEY_DMA_CHANNELS] = ["DMA1_Channel1"]
+        CORE.data[KEY_STM32][KEY_DMA_CHANNELS] = ["DMA1_Channel1", "DMA1_Channel2"]
     else:
         CORE.data[KEY_STM32][KEY_DMA_CHANNELS] = ["GPDMA1_Channel1", "GPDMA1_Channel0"]
 
