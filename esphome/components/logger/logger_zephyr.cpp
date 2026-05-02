0bd44401649c17e2cbeed7186274c78f75c0e013
Mariusz Kryński <mrk@sed.pl>
2026-05-03 01:02:42 +0200

    fix logger_zephyr

--- esphome/components/logger/logger_zephyr.cpp
+++ esphome/components/logger/logger_zephyr.cpp
@@ -163,7 +163,7 @@ void Logger::dump_crash_() {
   }
 }
 
-void k_sys_fatal_error_handler(unsigned int reason, const z_arch_esf_t *esf) {
+void k_sys_fatal_error_handler(unsigned int reason, const arch_esf *esf) {
   crash_buf.magic = App.get_config_hash();
   crash_buf.reason = reason;
   if (esf) {
@@ -187,7 +187,7 @@ void k_sys_fatal_error_handler(unsigned int reason, const z_arch_esf_t *esf) {
 
 extern "C" {
 
-void k_sys_fatal_error_handler(unsigned int reason, const z_arch_esf_t *esf) {
+void k_sys_fatal_error_handler(unsigned int reason, const arch_esf *esf) {
   esphome::logger::k_sys_fatal_error_handler(reason, esf);
 }
 }
